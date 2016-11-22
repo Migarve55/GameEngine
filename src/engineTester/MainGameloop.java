357f8fd33ef9aa18c6c917191096699a0f4b274e
Migarve55 <migarve55@gmail.com>
2016-11-22 22:18:28 +0100

    Fixed texture bug
    Fixed class renderer

--- src/engineTester/MainGameloop.java
+++ src/engineTester/MainGameloop.java
@@ -32,7 +32,7 @@ public class MainGameloop {
 		// Model + texture
 		TextureModel staticModel = new TextureModel(model,texture);
 		//Entity: model + texture + position
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
+		Entity entity = new Entity(staticModel, new Vector3f(0,-5,-20),0,0,0,1);
 		System.out.println("Models loaded");
 		Camera camera = new Camera();
 		System.out.println("Game started");
@@ -41,7 +41,7 @@ public class MainGameloop {
 		while(!Display.isCloseRequested()) {
 			//Game logic
 			entity.increasePosition(0f, 0f, 0f); //Moves the entity
-			entity.increaseRotation(0, 0.1f, 0); //Rotates it
+			entity.increaseRotation(0, 0.4f, 0); //Rotates it
 			camera.move(); //Move the camera
 			//Render
 			renderer.prepare(); //Change the background to green
